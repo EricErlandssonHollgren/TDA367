7ac70c684d7c934565e5607ed2454f1e0b022f77
Zaloz64 <zoe@zalo.se>
2022-09-19 09:03:14 +0200

    Done
    smal ajustments

--- core/src/com/tda367/game/App.java
+++ core/src/com/tda367/game/App.java
@@ -5,22 +5,42 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.tda367.game.Model.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class App extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
+	private BitmapFont font;
+	private MainHandler h1 = new Goldhandler();
+	private MainHandler h2 = new PointHandler();
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		h1.setSuccessor(h2);
+		// Send requests to the chain
+		h1.handleRequest(new Request(HandlerItemDefiners.POINTS, 100));
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		ScreenUtils.clear(0, 0, 0, 0);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, String.valueOf(((Goldhandler)h1).getGold()), 20, 430);
+		font.draw(batch, String.valueOf(((PointHandler)h2).getPoints()), 20, 450);
 		batch.end();
 	}
 	
@@ -28,5 +48,6 @@ public class App extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		font.dispose();
 	}
 }
--- core/src/com/tda367/game/Model/MainHandler.java
+++ core/src/com/tda367/game/Model/MainHandler.java
@@ -2,6 +2,7 @@ package com.tda367.game.Model;
 
 public abstract class MainHandler {
     protected MainHandler m_successor;
+
     public void setSuccessor(MainHandler successor)
     {
         m_successor = successor;
