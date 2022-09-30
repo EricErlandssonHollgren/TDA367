e3d64facb7316e588b28c87b8647a191c2560432
EricErlandssonHollgren <eric.e.hollgren@gmail.com>
2022-09-30 14:03:06 +0200

    ViewHolder updates

--- core/src/com/tda367/game/Model/ViewFactory.java
+++ core/src/com/tda367/game/Model/ViewFactory.java
@@ -3,7 +3,6 @@ package Model;
 import Interfaces.IProjectile;
 import Interfaces.IView;
 import View.ProjectileView;
-import com.badlogic.gdx.math.Vector2;
 
 public class ViewFactory {
     public static IView createProjectileView(IProjectile p){
