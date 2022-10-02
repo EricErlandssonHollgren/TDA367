f8a4d375317432e44074af3788513c0c48f560d1
Sally Chung <sallishsh@gmail.com>
2022-10-02 11:16:26 +0200

    Made createWalls and createGround() private.

--- core/src/com/tda367/game/Model/WorldBoundaries.java
+++ core/src/com/tda367/game/Model/WorldBoundaries.java
@@ -15,14 +15,11 @@ public class WorldBoundaries {
         return blocks;
     }
 
-    public void createGround(){
-        for (int i = 0; i < 100 ; i++) {
-            blocks.add(new Block(i,1, 100, 450));
-
-        }
+    private void createGround(){
+        blocks.add(new Block(0,1, 100, 1000));
     }
 
-    public void createWalls(){
+    private void createWalls(){
         blocks.add(new Block(315, 40, 400, 10));
         blocks.add(new Block(0,40,400,10));
     }
