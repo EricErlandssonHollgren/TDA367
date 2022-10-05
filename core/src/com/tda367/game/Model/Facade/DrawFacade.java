3d1e5fd175991039cd6b0ee42eb158879f06e227
Sally Chung <sallishsh@gmail.com>
2022-10-05 09:24:16 +0200

    Refactored worldBoundariesView so it holds a drawfacade.

--- core/src/com/tda367/game/Model/Facade/DrawFacade.java
+++ core/src/com/tda367/game/Model/Facade/DrawFacade.java
@@ -2,10 +2,14 @@ package Model.Facade;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 public class DrawFacade {
     private SpriteBatch batch;
     private Texture img;
+
+    private ShapeRenderer shaperenderer;
 
     public DrawFacade(String texturePath){
         batch = new SpriteBatch();
@@ -16,6 +20,11 @@ public class DrawFacade {
         batch.draw(img, x,y,imgWidth,imgHeight);
         batch.end();
     }
+
+    public ShapeRenderer getShaperenderer() {
+        return shaperenderer;
+    }
+
 
     public void dispose(){
         batch.dispose();
