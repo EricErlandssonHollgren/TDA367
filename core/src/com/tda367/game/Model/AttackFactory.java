13ceaa36a26bdd29de359d9bda2a971c737bc071
Amanda Papacosta <amanda.dyrell.papacosta@gmail.com>
2022-10-20 13:23:56 +0200

    Added JavaDoc for attackFactory

--- core/src/com/tda367/game/Model/AttackFactory.java
+++ core/src/com/tda367/game/Model/AttackFactory.java
@@ -4,6 +4,12 @@ import Interfaces.IEnemyAttack;
 
 public class AttackFactory {
 
+    /**
+     * Creates a fire attack and adds it to the list of fireAttacks in Entity Holder.
+     * @param x the x-position for the fire attack
+     * @param y the y-position for the fire attack
+     * @return the fire attack at a set position
+     */
     public static IEnemyAttack createFireFlame(float x, float y){
         FireAttack fireAttack = new FireAttack(x,y);
         EntityHolder.getInstance().addFireAttack(fireAttack);
