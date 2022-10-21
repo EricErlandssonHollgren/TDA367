0b636c250be0849680f38e6d6737359bbd4bbd06
EricErlandssonHollgren <eric.e.hollgren@gmail.com>
2022-10-21 10:56:57 +0200

    add AttackHitBoxTest

---
+++ core/src/test/java/AttackHitBoxTest.java
@@ -0,0 +1,43 @@
+import Model.AttackHitbox;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AttackHitBoxTest {
+
+    private AttackHitbox ah;
+
+    @BeforeEach
+    public void init(){
+        ah = new AttackHitbox(0,0);
+    }
+
+    @Test
+    public void getYTest(){
+        assertEquals(0,ah.getY());
+    }
+
+    @Test
+    public void getXTest(){
+        assertEquals(0,ah.getX());
+    }
+
+    @Test
+    public void getHeightTest(){
+        assertEquals(37,ah.getHeight());
+    }
+
+    @Test
+    public void getWidthTest(){
+        assertEquals(120,ah.getWidth());
+    }
+
+    @Test
+    public void setYTest(){
+        ah.setY(10);
+        assertEquals(10,ah.getY());
+    }
+}
